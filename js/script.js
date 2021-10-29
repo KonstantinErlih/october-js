78af8a31612dce57c5ae2d5b2fc93799e4217e49
Konstantin <erlih@bk.ru>
2021-10-29 19:37:29 +0300

    lesson12+

--- js/script.js
+++ js/script.js
@@ -1,27 +1,23 @@
 "use strict";
 
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
 console.log(numberOfFilms);
 console.log(typeof (numberOfFilms));
 
 const personalMovieDB = {
-    count: 0,
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-personalMovieDB.count = numberOfFilms;
+let film1 = prompt('Один из последних просмотренных фильмов?', '');
+let film1Rating = +prompt('На сколько оцените его?', '');
+let film2 = prompt('Один из последних просмотренных фильмов?', '');
+let film2Rating = +prompt('На сколько оцените его?', '');
 
-let film1 = prompt('Один из последних просмотренных фильмов?');
-let film1Rating = +prompt('На сколько оцените его?');
-let film2 = prompt('Один из последних просмотренных фильмов?');
-let film2Rating = +prompt('На сколько оцените его?');
-
-personalMovieDB.movies = {
-    film1: film1Rating,
-    film2: film2Rating
-};
+personalMovieDB.movies[film1] = film1Rating;
+personalMovieDB.movies[film2] = film2Rating;
 
 console.log(personalMovieDB);
