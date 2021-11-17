ed1797ace8fd038ff2f58dc60ef1338b69061447
Konstantin <erlih@bk.ru>
2021-11-17 12:01:17 +0300

    Lesson 24

--- js/script.js
+++ js/script.js
@@ -1,115 +1,64 @@
 "use strict";
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
-    }
-}
-
-// start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-};
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-            b = +prompt('На сколько оцените его?', '');
-        if (a != null && b != null && a != '' && b != '' && a.length <= 50) {
-            personalMovieDB.movies[a] = b;
+    privat: false,
+    start: function () {
+        personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", '');
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", '');
+        }
+    },
+    rememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', ''),
+                b = +prompt('На сколько оцените его?', '');
+            if (a != null && b != null && a != '' && b != '' && a.length <= 50) {
+                personalMovieDB.movies[a] = b;
+            } else {
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: () => {
+        if (personalMovieDB.count < 1) {
+            console.log('Произошла ошибка');
+        } else if (personalMovieDB.count < 10) {
+            console.log('Просмотрено довольно мало фильмов');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log('Вы классический зритель');
         } else {
-            i--;
+            console.log('Вы киноман');
+        }
+        console.log(personalMovieDB);
+    },
+    showMyDB: (hidden) => {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres: () => {
+        for (let i = 0; i < 3; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i + 1}`, '');
+            if (genre === '' || genre === null) {
+                i--;
+                console.log('Вы ввели некорректные данные');
+            } else {
+                personalMovieDB.genres[i] = genre;
+            }
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр номер ${i+1} - это ${item}`);
+        });
+    },
+    toggleVisibleMyDB: () => {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
         }
     }
-}
-
-// rememberMyFilms();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 1) {
-        console.log('Произошла ошибка');
-    } else if (personalMovieDB.count < 10) {
-        console.log('Просмотрено довольно мало фильмов');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log('Вы классический зритель');
-    } else {
-        console.log('Вы киноман');
-    }
-
-    console.log(personalMovieDB);
-}
-
-// detectPersonalLevel();
-
-function showMyDB() {
-    if (!personalMovieDB.privat) {
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB();
-
-function writeYourGenres() {
-    for (let i = 0; i < 3; i++) {
-        personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}`, '');
-    }
-}
-
-writeYourGenres();
-
-console.log(personalMovieDB);
-
-
-// callback функции
-// function call(ar1, callback) {
-//     console.log(ar1);
-// }
-// function two() {
-//     console.log('Вторая, но должна сработать первой');
-// }
-// call('Первая', two());
-
-// const str = 'Яблоко';
-// for (let symbol of str) {
-//     console.log(symbol);
-// }
-
-// // Клон объекта!!!
-// const q = {
-//     one: 1,
-//     two: 5
-// };
-
-// const qNew = { ...q };
-// qNew.one = 8;
-// console.log(q);
-// console.log(qNew);
-
-// Прототип solider -> 
-// сущуствующий объект john приобретает свойства прототипа
-// const solider = {
-//     health: 100,
-//     armor: 400
-// };
-// const john = {};
-
-// Object.setPrototypeOf(john, solider);
-
-// Новый объект по прототипу:
-// const solider = {
-//     health: 100,
-//     armor: 400
-// };
-// const john = Object.create(solider);
-
-
-
-
+};
