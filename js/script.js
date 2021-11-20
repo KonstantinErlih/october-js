f91187ffd181d7bbebc4e1b84b283be4f81f3014
Konstantin <erlih@bk.ru>
2021-11-20 20:21:21 +0300

    Lesson 29

--- js/script.js
+++ js/script.js
@@ -1,21 +1,39 @@
 'use strict';
-const box = document.getElementById('box');
-// console.log(box);
+const box = document.getElementById('box'),
+    btns = document.getElementsByTagName('button'),
+    circles = document.getElementsByClassName('circle'),
+    wrapper = document.querySelector('.wrapper'),
+    hearts = wrapper.querySelectorAll('.heart'),
+    oneHeart = wrapper.querySelector('.heart');
 
-// const btns = document.getElementsByTagName('button');
-// console.log(btns[1]);
 
-// const circles = document.getElementsByClassName('circle');
-// console.log(circles);
-
-//
-// Современные методы
-
-// const hearts = document.querySelectorAll('.heart');
-// console.log(hearts);
 // hearts.forEach(item => {
 //     console.log(item);
 // });
 
-// const oneHeart = document.querySelector('.heart');
-// console.log(oneHeart);
+box.style.backgroundColor = 'blue';
+box.style.width = '500px';
+box.style.cssText = 'background-color: green; width: 800px';
+
+btns[1].style.borderRadius = '50%';
+
+hearts.forEach(item => {
+    item.style.backgroundColor = 'orange';
+});
+
+circles[0].style.backgroundColor = 'red';
+
+const div = document.createElement('div');
+div.classList.add('black');
+document.body.append(div);
+// wrapper.prepend(div);
+// hearts[1].before(div);
+// hearts[1].after(div);
+// circles[0].remove();
+
+hearts[0].replaceWith(circles[0]); // hearts[0] заменить на circles[0]
+
+div.innerHTML = '<h3> Hello world </h3>';
+// div.textContent = 'Hello';
+
+div.insertAdjacentHTML('afterEnd', '<h2>Hello</h2>');
